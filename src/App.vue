21a38a0d184df70e1c0fda2711808c3a2571ebb8
vjblind <vjblind@gmail.com>
2021-01-08 05:57:25 +0100

    init template whiteFACE

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div class="jumbotron">
+  <h1 class="display-3">White Face</h1>
+ 
+
+
+   <img src="./assets/88a4068a30ca7f4f1d87b5d1.jpg" alt="Avatar" class="avatar"> 
+  <HelloWorld msg="Welcome to Your new you"         />
+
+ <p class="lead">"No more excuses, no delays"</p>
+  <hr class="my-2">
+  <p>More info</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Justice Now   </a>
+  </p>
+</div>
+
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+
+
+ 
+
+
 
 export default {
   name: 'App',
@@ -15,12 +34,25 @@ export default {
 </script>
 
 <style>
+.avatar {
+  vertical-align: middle;
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+}
+
+.jumbotron{
+
+ background: rgb(32, 32, 32);
+
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fafafa;
+  background: rgb(32, 32, 32);
   margin-top: 60px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,47 +1,115 @@
 <template>
+<div class="card  ">
+ 
+  <div class="card-body"> 
+       <h1>{{ msg }}</h1>
+    <h4 class="card-title">Name:{{name}}</h4>
+    <p class="card-text">Age:    {{ age }}</p>
+ 
   <div class="hello">
-    <h1>{{ msg }}</h1>
+
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    {{ story }}
+  
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <p>
+    {{ story1 }}
+  
+    </p>
+ 
+  </div> </div>
+</div>
+
+<div class="alert alert-dark" role="alert">
+  <a href="#" class="alert-link">An example of the the worst in the worl. But you ll
+  not face this fate if your white . This programme can spit infinity an story like this.   
+  Even if every date point change injustice will remain. There is still hope and you can change things.</a>. 
+</div>
 </template>
 
 <script>
+var age_info = 15;
+var tracery = require('tracery-grammar');
+ 
+
+
+var symbols ={ 
+  'age': ['19','15','23','44','21','26'], 
+  'name': ['Julius Jones','james','kerry','ina yosoun'],
+ 'relatives' : ['parents','girl friend','friend','mother']   ,        
+  'Activities' : ['watching TV','playing videos games','playing music','taking lunch'] ,
+  'Being' : ['football star','Just a kid','  being just like you','friend you ll never know'] ,
+   'sentence': ['19','15','23','44','21'],          }
+var dd = tracery.createGrammar(symbols);
+
+
+age_info= dd.flatten('#age#')
+var name_info= dd.flatten('#name#')
+var relatives_info= dd.flatten('#relatives#')
+var Activities_info= dd.flatten('#Activities#')
+var Being_info= dd.flatten('#Being#')
+var sentence_info= dd.flatten('#sentence#')
+
+var grammar = tracery.createGrammar({
+ 'animal': ['panda','fox','capybara','iguana'],
+'emotion': ['sad','happy','angry','jealous'],
+
+   
+
+  'origin1':['I am  '+name_info+', were he White Like Me.  '+name_info+' sits at home, '+Activities_info
+  +'  with his  '+relatives_info+'.'+ " " 
+  + ' The sound of laughter from a witty banter in the kitchen…—And then the police break in.'           ],
+
+  'origin2':['They take him at age '+age_info+'. '+ "   "
++'He was a  '+Being_info+' , full scholarship.'
+
++ "   "
++'He has been on Death Row for '+sentence_info+' years.'+ "   "
++ 
+'He is now in solitary confinement. '+ "   "+'He was miles away from the crime scene.'+
+ "   "+'The jury was certain a black man did it.'   
+
+],
+
+
+   
+});
+
+ 
+grammar.addModifiers(tracery.baseEngModifiers); 
+
+ //var result=gramar.flatten( " salut    #name#         ");
+
+
 export default {
   name: 'HelloWorld',
+
+  data() {
+    return {            
+      
+      
+      story:grammar.flatten('#origin1#'),
+      story1:grammar.flatten('#origin2#'),
+      age:age_info,
+      name:name_info
+      
+      }},
+
   props: {
-    msg: String
+    msg: String,
+   
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.card{background: rgb(35, 36, 35);}
+.hello{
+
+background: rgb(35, 36, 35);
+}
 h3 {
   margin: 40px 0 0;
 }
